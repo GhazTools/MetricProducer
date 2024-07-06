0df0fc4e9813ffca9bd0cb4a2f07ce69a7dd813b
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-07-06 00:33:00 -0400

    update logic

---
+++ lib/metric_producer/include/metric_producer.hpp
@@ -0,0 +1,29 @@
+// STL IMPORTS START HERE
+#include <iostream>
+#include <cstdlib>
+// STL IMPORTS END HERE
+
+// THIRD PARTY IMPORTS START HERE
+#include <librdkafka/rdkafkacpp.h>
+
+#include "../../json.hpp"
+// THIRD PARTY IMPORTS END HERE
+
+
+class MetricProducer 
+{
+public:
+MetricProducer();
+~MetricProducer();
+
+
+private:
+RdKafka::Producer* _producer;
+RdKafka::Topic* _topic;
+
+RdKafka::Conf* getConfig_() const;
+RdKafka::Producer* getProducer_(const RdKafka::Conf* conf) const;
+RdKafka::Topic* getTopic_() const;
+
+std::string getEnvironmentVariable_(const char* name) const;
+};
